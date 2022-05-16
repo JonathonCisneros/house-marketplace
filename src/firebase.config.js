7134c7a614f6792c7eb23bba09aae9d8e8d1a808
Jonathon Cisneros <38201359+JonathonCisneros@users.noreply.github.com>
2022-05-16 07:00:13 -0700

    setting up pages, some assets and basics

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBc8kYdBpgyseT2qhAY5Poa94KVWVAjRko",
+  authDomain: "house-marketplace-app-f10a3.firebaseapp.com",
+  projectId: "house-marketplace-app-f10a3",
+  storageBucket: "house-marketplace-app-f10a3.appspot.com",
+  messagingSenderId: "947396163963",
+  appId: "1:947396163963:web:4234f3096c5f18f10ea5de"
+};
+
+export const db = getFirestore( ); 
