3c1d85f7416a7314ee1e085e59b21785f7352ae0
Jonathon Cisneros <38201359+JonathonCisneros@users.noreply.github.com>
2022-05-17 06:26:36 -0700

    log out, display profile details and update profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,96 @@
 import { useEffect, useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogOut = () => {
+    auth.signOut();
+    navigate('/');
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update name in firebase database
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update in firebase firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error('Could not update profile details');
+    }
+  };
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
   return (
-    <div>
-      <h1>{user ? user.displayName : 'User Not Found'}</h1>
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogOut}>
+          Log Out
+        </button>
+      </header>
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+            className='changePersonalDetails'
+          >
+            {changeDetails ? 'Done' : 'Change'}
+          </p>
+        </div>
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type='email'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,6 +8,7 @@ import {
 } from 'firebase/auth';
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 function SignUp() {
@@ -57,7 +58,7 @@ function SignUp() {
 
       navigate('/');
     } catch (error) {
-      console.log(error);
+      toast.error('Something went wrong with registration');
     }
   };
 
